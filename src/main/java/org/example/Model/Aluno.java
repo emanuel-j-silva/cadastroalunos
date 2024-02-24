149cc47c105b7d25e46e91d193235c3e80926fc6
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-02-24 13:18:46 -0300

    Atualização do model

--- src/main/java/org/example/Model/Aluno.java
+++ src/main/java/org/example/Model/Aluno.java
@@ -10,4 +10,64 @@ public class Aluno {
     private BigDecimal nota1;
     private BigDecimal nota2;
     private BigDecimal nota3;
+
+    // Construtor default
+    public Aluno(){}
+
+    public Aluno(String nome, String ra, String email, BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
+        this.nome = nome;
+        this.ra = ra;
+        this.email = email;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRa() {
+        return ra;
+    }
+
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public BigDecimal getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(BigDecimal nota1) {
+        this.nota1 = nota1;
+    }
+
+    public BigDecimal getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(BigDecimal nota2) {
+        this.nota2 = nota2;
+    }
+
+    public BigDecimal getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(BigDecimal nota3) {
+        this.nota3 = nota3;
+    }
 }
